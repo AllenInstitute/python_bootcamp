a49fab3a842ec72600cf330ec9c65b9bbb1d2890
Luke Campagnola <luke.campagnola@gmail.com>
2024-08-17 01:31:45 -0700

    Move new 05 code to solutions file

--- code/solutions/my_package/preprocessing.py
+++ code/solutions/my_package/preprocessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def downsample(data, n, axis=0):
     """Reduce the number of samples in an array by averaging together
     n samples at a time.
@@ -16,3 +17,11 @@ def downsample(data, n, axis=0):
     d1.shape = tuple(s)
     d2 = d1.mean(axis+1)
     return d2
+
+
+def normalize(data, axis=0):
+    """Normalize an array along a given axis to have a mean of 0 and standard deviation of 1."""
+    mean = np.mean(data, axis=axis, keepdims=True)
+    std = np.std(data, axis=axis, keepdims=True)
+    normalized_data = (data - mean) / std
+    return normalized_data
